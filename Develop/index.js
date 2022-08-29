f310cb93fb1c3284da608930849e294a6173d6e7
Daniel Xu <xudh47@gmail.com>
2022-08-28 23:22:10 -0700

    Created path to generateMarkdown

---
+++ Develop/index.js
@@ -0,0 +1,19 @@
+// TODO: Include packages needed for this application
+const path = require("path");
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+const generateMarkdown = require("./utils/generateMarkdown");
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {
+  console.log("test");
+}
+
+// Function call to initialize app
+init();
