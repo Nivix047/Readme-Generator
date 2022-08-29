dedb695531467c088ab561f54a7921594a6123c5
Daniel Xu <xudh47@gmail.com>
2022-08-28 23:27:08 -0700

    Created questions array

--- Develop/index.js
+++ Develop/index.js
@@ -5,7 +5,39 @@ const fs = require("fs");
 
 const generateMarkdown = require("./utils/generateMarkdown");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title of the project?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Provide a short description of the project",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "What type of license does your project have?",
+    choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "none"],
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "What are the steps required to install your project if any",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Provide instructions and examples for use",
+  },
+  {
+    type: "input",
+    name: "credits",
+    message: "List your collaborators",
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
